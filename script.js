1c03daaac351daf04c40d2389cacd0d93d9b4fa4
devbyjen388 <jenniferdinh38@gmail.com>
2026-02-28 23:29:19 -0600

    text updates to index

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const contact_link = document.getElementById("contact_link");
+  const works_link = document.getElementById("works_link");
+  const proj_works_link = document.getElementById("proj_works_link");
   const contact_overlay = document.getElementById("contact_overlay");
+  const works_overlay = document.getElementById("works_overlay");
   const hamburger = document.getElementById("hamburger");
   const nav = document.getElementById("nav");
-  const close_contact_form = document.getElementById("close_button");
+  const close_contact_form = document.getElementById("close_button_form");
 
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -12,8 +15,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
   contact_link.addEventListener("click", (e) => {
     e.preventDefault();
+
+    if (works_overlay.classList.contains("active")) {
+      works_overlay.classList.remove("active");
+    }
+
     contact_overlay.classList.toggle("active");
     document.body.classList.add("no-scroll");
+  });
+
+  proj_works_link.addEventListener("click", (e) => {
+    works_overlay.classList.toggle("active");
+    document.body.classList.remove("no-scroll");
+  });
+
+  works_link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (contact_overlay.classList.contains("active")) {
+      contact_overlay.classList.remove("active");
+    }
+
+    works_overlay.classList.toggle("active");
+
+    if (works_overlay.classList.contains("active")) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
   });
 
   close_contact_form.addEventListener("click", () => {
